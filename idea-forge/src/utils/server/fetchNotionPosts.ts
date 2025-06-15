d5a822ebdd04f6950e52d5b362eefc7c168ad88d
rjsdn031 <geonwoo031@gmail.com>
2025-06-15 18:43:30 +0900

    [refactor] Delete Unused codes

--- idea-forge/src/utils/server/fetchNotionPosts.ts
+++ idea-forge/src/utils/server/fetchNotionPosts.ts
@@ -1,8 +1,6 @@
 import { Client } from '@notionhq/client';
 import type {
   PageObjectResponse,
-  MultiSelectPropertyItemObjectResponse,
-  RichTextItemResponse,
 } from '@notionhq/client/build/src/api-endpoints';
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
