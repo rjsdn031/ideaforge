688e5d386ed740c44c426a28a3be283c7bd2aaa3
rjsdn031 <geonwoo031@gmail.com>
2025-06-26 15:32:07 +0900

    [hotfix] Fix Equation Display

--- idea-forge/src/components/PostBody.tsx
+++ idea-forge/src/components/PostBody.tsx
@@ -7,6 +7,8 @@ import type { ExtendedRecordMap } from 'notion-types';
 import { Code } from 'react-notion-x/build/third-party/code';
 import { Equation } from 'react-notion-x/build/third-party/equation';
 import { Modal } from 'react-notion-x/build/third-party/modal';
+import { Collection } from 'react-notion-x/build/third-party/collection';
+import { Pdf } from 'react-notion-x/build/third-party/pdf';
 
 import styles from '../styles/PostBody.module.css';
 
@@ -41,10 +43,12 @@ const PostBody = ({ recordMap }: PostBodyProps) => {
         fullPage={false}
         darkMode={isDarkMode}
         components={{
-          Code,
-          Equation,
-          Modal,
-        }}
+      Code,
+      Collection,
+      Equation,
+      Modal,
+      Pdf
+    }}
       />
     </div>
   );
