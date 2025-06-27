9f03c0ed787c5c134dc029cfdf58819051bcce4e
rjsdn031 <geonwoo031@gmail.com>
2025-06-27 18:08:37 +0900

    [fix] Mod footer

--- idea-forge/src/components/Footer.tsx
+++ idea-forge/src/components/Footer.tsx
@@ -5,6 +5,10 @@ import styles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <div className={styles.banners}>
+        <a href="https://github.com/rjsdn031">github</a>
+        <a href="https://example.com">CV</a>
+      </div>
       <p>© {new Date().getFullYear()} Geonwoo Choi. All rights reserved.</p>
     </footer>
   );
